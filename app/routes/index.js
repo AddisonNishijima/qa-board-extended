16a4eaf63c457fbee00a034dac97d230ba178433
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 09:23:55 -0700

    add new post to route handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,12 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    savePost3(params) {
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('index');
+    },
+    
     destroyPost(post) {
       post.destroyRecord();
       this.transitionTo('index');
