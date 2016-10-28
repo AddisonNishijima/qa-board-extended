2a396da4a8b9e90b9e52654e74bec04a11bd7f30
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 08:15:38 -0700

    add hard coded data for posts to index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+var posts = [{
+  id: 1,
+  author: "NewtoCode",
+  question: "What does HTML stand for?"
+}, {
+  id: 2,
+  author: "CSSNewbie100",
+  question: "What does CSS stand for?"
+}, {
+  id: 3,
+  author: "MontyPythonRocks",
+  question: "What is the air-speed velocity of an unladen swallow?"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return posts;
+  },
 });
