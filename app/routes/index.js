af8d790787b7247d908f2b0b5592fd96f57662cd
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 08:26:45 -0700

    add notes attribute to model and hardcoded data

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,18 @@ import Ember from 'ember';
 var posts = [{
   id: 1,
   author: "NewtoCode",
-  question: "What does HTML stand for?"
+  question: "What does HTML stand for?",
+  notes: "N/A"
 }, {
   id: 2,
   author: "CSSNewbie100",
-  question: "What does CSS stand for?"
+  question: "What does CSS stand for?",
+  notes: "N/A"
 }, {
   id: 3,
   author: "MontyPythonRocks",
-  question: "What is the air-speed velocity of an unladen swallow?"
+  question: "What is the air-speed velocity of an unladen swallow?",
+  notes: "N/A"
 }];
 
 export default Ember.Route.extend({
