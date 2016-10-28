1b7403ad64a340e0e146d214bc5e44fb22a2abe0
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 09:06:48 -0700

    add new post behavior to new-post.js

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow() {
+      this.set('addNewPost', true);
+    }
+  }
 });
