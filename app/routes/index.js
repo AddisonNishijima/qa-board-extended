df4d24dc306bf77c000522d4cb40b41e9905b2aa
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 08:40:32 -0700

    update model hook in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,7 @@
 import Ember from 'ember';
 
-var posts = [{
-  id: 1,
-  author: "NewtoCode",
-  question: "What does HTML stand for?",
-  notes: "N/A"
-}, {
-  id: 2,
-  author: "CSSNewbie100",
-  question: "What does CSS stand for?",
-  notes: "N/A"
-}, {
-  id: 3,
-  author: "MontyPythonRocks",
-  question: "What is the air-speed velocity of an unladen swallow?",
-  notes: "N/A"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return posts;
+    return this.store.findAll('post');
   },
 });
