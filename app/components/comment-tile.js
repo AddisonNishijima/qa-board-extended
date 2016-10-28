57748b471f2b302c687a5dbf99dae84bfa7d91da
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-28 14:34:06 -0700

    remove unnecessary div containers from all pages, modify background images, add and modify css styling

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    delete(review) {
+    delete(comment) {
       if (confirm('Are you sure you want to delete this comment?')) {
         this.sendAction('destroyComment', comment);
       }
