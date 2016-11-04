fbeb14edf915979add1b22a028ac570ad01455b8
Addison Nishijima <addison.nishijima@gmail.com>
2016-11-04 12:58:58 -0700

    attempt to get service to work

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -3,9 +3,4 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoritePost: Ember.inject.service(),
 
-  actions: {
-    addToFavorite(favorite) {
-      this.get('favoritePost').add(favorite);
-    }
-  }
 });
