08474dc19147546ea99ed826488b7216cf89e733
Addison Nishijima <addison.nishijima@gmail.com>
2016-11-04 09:50:12 -0700

    attempt to get favorites list to display

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('comment.commenter', 'comment.content', function() {
+    return this.get('comment.commenter') + ' - ' + this.get('comment.content');
+  }),
+
   actions: {
     delete(comment) {
       if (confirm('Are you sure you want to delete this comment?')) {
